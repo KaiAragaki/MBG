c72b69691832207403c9c381e55b12ae99b3a3fa
KaiAragaki <gens_caches@cox.net>
2018-10-13 17:49:49 -0400

    Working on turning cubes into squares...

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # MBG
+
+## getCodonProbabilities
+
+### Description
 Get codon probabilities shows what the probability of codons will be given a certain GC content (default provided) and then what average peptide mass would be expected.
 
+### Ramblings
 Initially I wanted the function to pass the codon cube through a eukaryote amino acid frequency cube, but I decided against it: That process would also have taken into account the GC content of the organism used for the eukaryote amino acid frequency cube itself, not only organism in the question.
+
+### Requirements
+Tidyverse (Specifically plyr)
+Bioconductor's "Biostrings"
--- getCodonProbabilities.R
+++ getCodonProbabilities.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(plyr)
+library(Biostrings)
 getCodonProbabilities <- function(A = .125, C = .325, G = .325, U = .125) {
   rowNames <- c("A","C","G","U")
   columnNames <- c("A", "C", "G", "U")
@@ -19,5 +21,23 @@ getCodonProbabilities <- function(A = .125, C = .325, G = .325, U = .125) {
   threeNucProb[1,,] <- threeNucProb[1,,]*aArray
   threeNucProb[2,,] <- threeNucProb[2,,]*cArray
   threeNucProb[3,,] <- threeNucProb[3,,]*gArray
-  threeNucProb[4,,] <- threeNucProb[4,,]*uArray
-}  
+  threeNucProb[4,,] <- threeNucProb[4,,]*uArray # Yeah, I get it. This is a loop just waiting to be made. That takes brain power.
+  threeNucProb <<- threeNucProb # Push to global environment so I can prod it without rerunning the code
+}
+# TODO
+# Generate codon LUT
+# Index by codon name
+
+# New New Plan:
+# Call codons one at a time (lapply?)
+# and have them do the penguin mother reunion "are you my mommy" call
+# Bioconductor finds its child and tells it what amino acid it is, stores its probability
+# Masses
+
+for (i in 1:4){
+  for (j in 1:4){
+    for (k in 1:4){
+      print(names(threeNucProb[i,j,k])) # Did I get the order right?
+    }
+  }
+}
